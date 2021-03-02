53941bb84b80fa62ccda8a440af098cdcd965db4
Braeze <tobias0612@live.dk>
2021-03-02 12:09:40 +0100

    uploader til linux
    mere trial and error

--- sumo-rl/ql_2way-single-intersection.py
+++ sumo-rl/ql_2way-single-intersection.py
@@ -16,23 +16,25 @@ from sumo_rl.exploration import EpsilonGreedy
 
 
 if __name__ == '__main__':
-
-    prs = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    try:
+        prs = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter,
                                   description="""Q-Learning Single-Intersection""")
-    prs.add_argument("-route", dest="route", type=str, default='nets/2way-single-intersection/single-intersection-vhvh.rou.xml', help="Route definition xml file.\n")
-    prs.add_argument("-a", dest="alpha", type=float, default=0.1, required=False, help="Alpha learning rate.\n")
-    prs.add_argument("-g", dest="gamma", type=float, default=0.99, required=False, help="Gamma discount rate.\n")
-    prs.add_argument("-e", dest="epsilon", type=float, default=0.05, required=False, help="Epsilon.\n")
-    prs.add_argument("-me", dest="min_epsilon", type=float, default=0.005, required=False, help="Minimum epsilon.\n")
-    prs.add_argument("-d", dest="decay", type=float, default=1.0, required=False, help="Epsilon decay.\n")
-    prs.add_argument("-mingreen", dest="min_green", type=int, default=10, required=False, help="Minimum green time.\n")
-    prs.add_argument("-maxgreen", dest="max_green", type=int, default=30, required=False, help="Maximum green time.\n")
-    prs.add_argument("-gui", action="store_true", default=True, help="Run with visualization on SUMO.\n")
-    prs.add_argument("-fixed", action="store_true", default=False, help="Run with fixed timing traffic signals.\n")
-    prs.add_argument("-s", dest="seconds", type=int, default=1000, required=False, help="Number of simulation seconds.\n")
-    prs.add_argument("-r", dest="reward", type=str, default='wait', required=False, help="Reward function: [-r queue] for average queue reward or [-r wait] for waiting time reward.\n")
-    prs.add_argument("-runs", dest="runs", type=int, default=1, help="Number of runs.\n")
-    args = prs.parse_args()
+        prs.add_argument("-route", dest="route", type=str, default='nets/2way-single-intersection/single-intersection-vhvh.rou.xml', help="Route definition xml file.\n")
+        prs.add_argument("-a", dest="alpha", type=float, default=0.1, required=False, help="Alpha learning rate.\n")
+        prs.add_argument("-g", dest="gamma", type=float, default=0.99, required=False, help="Gamma discount rate.\n")
+        prs.add_argument("-e", dest="epsilon", type=float, default=0.05, required=False, help="Epsilon.\n")
+        prs.add_argument("-me", dest="min_epsilon", type=float, default=0.005, required=False, help="Minimum epsilon.\n")
+        prs.add_argument("-d", dest="decay", type=float, default=1.0, required=False, help="Epsilon decay.\n")
+        prs.add_argument("-mingreen", dest="min_green", type=int, default=10, required=False, help="Minimum green time.\n")
+        prs.add_argument("-maxgreen", dest="max_green", type=int, default=30, required=False, help="Maximum green time.\n")
+        prs.add_argument("-gui", action="store_true", default=False, help="Run with visualization on SUMO.\n")
+        prs.add_argument("-fixed", action="store_true", default=False, help="Run with fixed timing traffic signals.\n")
+        prs.add_argument("-s", dest="seconds", type=int, default=10000, required=False, help="Number of simulation seconds.\n")
+        prs.add_argument("-r", dest="reward", type=str, default='wait', required=False, help="Reward function: [-r queue] for average queue reward or [-r wait] for waiting time reward.\n")
+        prs.add_argument("-runs", dest="runs", type=int, default=1, help="Number of runs.\n")
+        args = prs.parse_args()
+    except BaseException:
+        print("args")
     experiment_time = str(datetime.now()).split('.')[0]
     out_csv = 'outputs/2way-single-intersection/a'
 
@@ -53,6 +55,7 @@ if __name__ == '__main__':
                                  alpha=args.alpha,
                                  gamma=args.gamma,
                                  exploration_strategy=EpsilonGreedy(initial_epsilon=args.epsilon, min_epsilon=args.min_epsilon, decay=args.decay)) for ts in env.ts_ids}
+        print(ql_agents)
 
         done = {'__all__': False}
         infos = []
