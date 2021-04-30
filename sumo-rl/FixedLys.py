c4bd30888d2beef8c7bb9567b6514314e8ef749e
Braeze <tobias0612@live.dk>
2021-04-30 09:38:23 +0200

    1.0
    model virker og fixed lys fungere også

--- sumo-rl/FixedLys.py
+++ sumo-rl/FixedLys.py
@@ -16,35 +16,37 @@ from sumo_rl.exploration import EpsilonGreedy
 
 
 if __name__ == '__main__':
-
-    prs = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    try:
+        prs = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter,
                                   description="""Q-Learning Single-Intersection""")
-    prs.add_argument("-route", dest="route", type=str, default='nets/double/flow.rou.xml', help="Route definition xml file.\n")
-    prs.add_argument("-a", dest="alpha", type=float, default=0.1, required=False, help="Alpha learning rate.\n")
-    prs.add_argument("-g", dest="gamma", type=float, default=0.99, required=False, help="Gamma discount rate.\n")
-    prs.add_argument("-e", dest="epsilon", type=float, default=0.05, required=False, help="Epsilon.\n")
-    prs.add_argument("-me", dest="min_epsilon", type=float, default=0.005, required=False, help="Minimum epsilon.\n")
-    prs.add_argument("-d", dest="decay", type=float, default=1.0, required=False, help="Epsilon decay.\n")
-    prs.add_argument("-mingreen", dest="min_green", type=int, default=5, required=False, help="Minimum green time.\n")
-    prs.add_argument("-maxgreen", dest="max_green", type=int, default=300, required=False, help="Maximum green time.\n")
-    prs.add_argument("-gui", action="store_true", default=True, help="Run with visualization on SUMO.\n")
-    prs.add_argument("-fixed", action="store_true", default=True, help="Run with fixed timing traffic signals.\n")
-    prs.add_argument("-s", dest="seconds", type=int, default=30000, required=False, help="Number of simulation seconds.\n")
-    prs.add_argument("-r", dest="reward", type=str, default='wait', required=False, help="Reward function: [-r queue] for average queue reward or [-r wait] for waiting time reward.\n")
-    prs.add_argument("-runs", dest="runs", type=int, default=1, help="Number of runs.\n")
-    args = prs.parse_args()
+        prs.add_argument("-route", dest="route", type=str, default='nets/2way-single-intersection/single-intersection-vhvh.rou.xml', help="Route definition xml file.\n")
+        prs.add_argument("-a", dest="alpha", type=float, default=0.1, required=False, help="Alpha learning rate.\n")
+        prs.add_argument("-g", dest="gamma", type=float, default=0.99, required=False, help="Gamma discount rate.\n")
+        prs.add_argument("-e", dest="epsilon", type=float, default=0.05, required=False, help="Epsilon.\n")
+        prs.add_argument("-me", dest="min_epsilon", type=float, default=0.005, required=False, help="Minimum epsilon.\n")
+        prs.add_argument("-d", dest="decay", type=float, default=1.0, required=False, help="Epsilon decay.\n")
+        prs.add_argument("-mingreen", dest="min_green", type=int, default=5, required=False, help="Minimum green time.\n")
+        prs.add_argument("-maxgreen", dest="max_green", type=int, default=60, required=False, help="Maximum green time.\n")
+        prs.add_argument("-gui", action="store_true", default=True, help="Run with visualization on SUMO.\n")
+        prs.add_argument("-fixed", action="store_true", default=True, help="Run with fixed timing traffic signals.\n")
+        prs.add_argument("-s", dest="seconds", type=int, default=100000, required=False, help="Number of simulation seconds.\n")
+        prs.add_argument("-r", dest="reward", type=str, default='wait', required=False, help="Reward function: [-r queue] for average queue reward or [-r wait] for waiting time reward.\n")
+        prs.add_argument("-runs", dest="runs", type=int, default=1, help="Number of runs.\n")
+        args = prs.parse_args()
+    except BaseException:
+        print(prs)
     experiment_time = str(datetime.now()).split('.')[0]
-    out_csv = 'outputs/Fixed/tid'
+    out_csv = 'outputs/Fixed/min_green_%s_max_green_%s' % (args.min_green,args.max_green)
 
-    env = SumoEnvironment(net_file='nets/double/network.net.xml',
-                        route_file=args.route,
-                        out_csv_name=out_csv,
-                        use_gui=args.gui,
-                        num_seconds=args.seconds,
-                        min_green=args.min_green,
-                        max_green=args.max_green,
-                        max_depart_delay=0)
-                        
+    env = SumoEnvironment(net_file='nets/2way-single-intersection/single-intersection.net.xml',
+                          route_file=args.route,
+                          out_csv_name=out_csv,
+                          use_gui=args.gui, 
+                          num_seconds=args.seconds,
+                          min_green=args.min_green,
+                          max_green=args.max_green,
+                          max_depart_delay=0)
+
     for run in range(1, args.runs+1):
         initial_states = env.reset()
         ql_agents = {ts: QLAgent(starting_state=env.encode(initial_states[ts], ts),
@@ -53,6 +55,7 @@ if __name__ == '__main__':
                                  alpha=args.alpha,
                                  gamma=args.gamma,
                                  exploration_strategy=EpsilonGreedy(initial_epsilon=args.epsilon, min_epsilon=args.min_epsilon, decay=args.decay)) for ts in env.ts_ids}
+        print(ql_agents)
 
         done = {'__all__': False}
         infos = []
@@ -69,3 +72,7 @@ if __name__ == '__main__':
                     ql_agents[agent_id].learn(next_state=env.encode(s[agent_id], agent_id), reward=r[agent_id])
         env.save_csv(out_csv, run)
         env.close()
+
+
+
+
--- sumo-rl/Force.py
+++ sumo-rl/Force.py
@@ -16,10 +16,10 @@ from ray.rllib.agents.dqn.dqn import DQNTrainer
 from ray.rllib.agents.dqn.dqn_tf_policy import DQNTFPolicy
 from ray.rllib.agents.trainer import with_common_config
 
-
 from ray.rllib.agents.a3c.a3c_tf_policy import A3CTFPolicy
 from ray.tune.registry import register_env
 from gym import spaces
+import gym
 import numpy as np
 from sumo_rl import SumoEnvironment
 import traci
@@ -148,26 +148,224 @@ DEFAULT_CONFIG = with_common_config({
 # __sphinx_doc_end__
 # yapf: enable
 
+#python outputs/plot.py -f outputs/test/test_run2 
 
 if __name__ == '__main__':
     ray.init()
 
-    register_env("4x4grid", lambda _: SumoEnvironment(net_file='nets/double/network.net.xml',
-                                                    route_file='nets/double/flow.rou.xml',
-                                                    out_csv_name='outputs/Data/Lidqn',
-                                                    use_gui=True,
+#register_env("multienv", lambda config: MultiEnv(config))
+
+    register_env("4x4grid", lambda _: SumoEnvironment(net_file='nets/2way-single-intersection/single-intersection.net.xml',
+                                                    route_file='nets/2way-single-intersection/single-intersection-gen.rou.xml',
+                                                    out_csv_name='outputs/test/test',
+                                                    use_gui=False,
                                                     num_seconds=80000,
                                                     yellow_time=4,
                                                     min_green=5,
+                                                    max_green=60,
+                                                    max_depart_delay=0))
+    '''                                                
+    env = SumoEnvironment(SumoEnvironment(net_file='nets/double/network.net.xml',
+                                                    route_file='nets/double/flow.rou.xml',
+                                                    out_csv_name='outputs/Data/testt',
+                                                    use_gui=True,
+                                                    num_seconds=5000,
+                                                    yellow_time=4,
+                                                    min_green=5,
                                                     max_green=300,
-                                                    max_depart_delay=0.5))
+                                                    max_depart_delay=300))
+    '''
+                                                    
+                                                    
 
 
     start = time.time()
     print("hello")
-    
-    trainer = DQNTrainer(config=DEFAULT_CONFIG, env='4x4grid')
+    i = 0
+    trainer = DQNTrainer(config=DEFAULT_CONFIG, env="4x4grid")
+    #trainer = DQNTrainer(env="4x4grid", config={
+    #    "multiagent": {
+    #        "policies": {
+    #            '0': (DQNTrainer, spaces.Box(low=np.zeros(10), high=np.ones(10)), spaces.Discrete(2), {})
+    #        },
+    #        "policy_mapping_fn": (lambda id: '0')  # Traffic lights are always controlled by this policy
+    #    },
+    #    "lr": 0.001,
+    #    "no_done_at_end": True
+    #    })
+    #trainer.restore("model/saved")
     while True:
-        print(trainer.train())  # distributed training step
+        i +=1
+        print(i)
+        print(i)
+        print(i)
+        print(i)
+        print(i)
+        print(i)
+        print(i)
+        results = trainer.train()
+        print(results)  # distributed training step
+        if (i%10 == 0):
+            #trainer.save("model/saved")
+            break
+    print("er vi her????") 
+   
+
     end = time.time()
     print(end - start)
+
+    print("Vi  er her ")
+    print("Vi  er her ")
+    print("Vi  er her ")
+    print("Vi  er her ")
+    
+    ny_config = with_common_config({
+        # === Model ===
+        # Number of atoms for representing the distribution of return. When
+        # this is greater than 1, distributional Q-learning is used.
+        # the discrete supports are bounded by v_min and v_max
+        "num_atoms": 1,
+        "v_min": -10.0,
+        "v_max": 10.0,
+        # Whether to use noisy network
+        "noisy": False,
+        # control the initial value of noisy nets
+        "sigma0": 0.5,
+        # Whether to use dueling dqn
+        "dueling": True,
+        # Dense-layer setup for each the advantage branch and the value branch
+        # in a dueling architecture.
+        "hiddens": [256],
+        # Whether to use double dqn
+        "double_q": True,
+        # N-step Q learning
+        "n_step": 1,
+
+        # === Exploration Settings ===
+        "exploration_config": {
+            # The Exploration class to use.
+            "type": "EpsilonGreedy",
+            # Config for the Exploration class' constructor:
+            "initial_epsilon": 100.0,
+            "final_epsilon": 1000.0,
+            "epsilon_timesteps": 10000,  # Timesteps over which to anneal epsilon.
+
+            # For soft_q, use:
+            # "exploration_config" = {
+            #   "type": "SoftQ"
+            #   "temperature": [float, e.g. 1.0]
+            # }
+        },
+        # Switch to greedy actions in evaluation workers.
+        "evaluation_config": {
+            "explore": False,
+        },
+
+        # Minimum env steps to optimize for per train call. This value does
+        # not affect learning, only the length of iterations.
+        "timesteps_per_iteration": 1000,
+        # Update the target network every `target_network_update_freq` steps.
+        "target_network_update_freq": 500,
+        # === Replay buffer ===
+        # Size of the replay buffer. Note that if async_updates is set, then
+        # each worker will have a replay buffer of this size.
+        "buffer_size": 50000,
+        # The number of contiguous environment steps to replay at once. This may
+        # be set to greater than 1 to support recurrent models.
+        "replay_sequence_length": 1,
+        # If True prioritized replay buffer will be used.
+        "prioritized_replay": True,
+        # Alpha parameter for prioritized replay buffer.
+        "prioritized_replay_alpha": 0.6,
+        # Beta parameter for sampling from prioritized replay buffer.
+        "prioritized_replay_beta": 0.4,
+        # Final value of beta (by default, we use constant beta=0.4).
+        "final_prioritized_replay_beta": 0.4,
+        # Time steps over which the beta parameter is annealed.
+        "prioritized_replay_beta_annealing_timesteps": 20000,
+        # Epsilon to add to the TD errors when updating priorities.
+        "prioritized_replay_eps": 1e-6,
+
+        # Whether to LZ4 compress observations
+        "compress_observations": False,
+        # Callback to run before learning on a multi-agent batch of experiences.
+        "before_learn_on_batch": None,
+        # If set, this will fix the ratio of replayed from a buffer and learned on
+        # timesteps to sampled from an environment and stored in the replay buffer
+        # timesteps. Otherwise, the replay will proceed at the native ratio
+        # determined by (train_batch_size / rollout_fragment_length).
+        "training_intensity": None,
+
+        # === Optimization ===
+        # Learning rate for adam optimizer
+        "lr": 0.001,
+        # Learning rate schedule
+        "lr_schedule": None,
+        # Adam epsilon hyper parameter
+        "adam_epsilon": 1e-8,
+        # If not None, clip gradients during optimization at this value
+        "grad_clip": 40,
+        # How many steps of the model to sample before learning starts.
+        "learning_starts": 1000,
+        # Update the replay buffer with this many samples at once. Note that
+        # this setting applies per-worker if num_workers > 1.
+        "rollout_fragment_length": 4,
+        # Size of a batch sampled from replay buffer for training. Note that
+        # if async_updates is set, then each worker returns gradients for a
+        # batch of this size.
+        "train_batch_size": 32,
+
+        # === Parallelism ===
+        # Number of workers for collecting samples with. This only makes sense
+        # to increase if your environment is particularly slow to sample, or if
+        # you"re using the Async or Ape-X optimizers.
+        "num_workers": 0,
+        # Whether to compute priorities on workers.
+        "worker_side_prioritization": False,
+        # Prevent iterations from going lower than this time span
+        "min_iter_time_s": 1,
+        "no_done_at_end": True,
+
+    })
+    done = DQNTrainer(config=ny_config, env="4x4grid")       
+    done.restore("model/saved/checkpoint_5/checkpoint-5")
+    #done.import_model("my_weights.h5")
+    færdig_model= done.train()
+    print(færdig_model)
+
+
+
+
+
+    #policy.model.base_model.summary()
+    #Model: "model"
+
+    # instantiate env class
+    
+    # run until episode ends
+    #obs = env.reset()
+    #episode_reward = 0
+    #done = False
+    #while not done:
+    #    action = trainer.compute_action(obs)
+    #    obs, reward, done, info = env.step(action)
+    #    episode_reward += reward
+    
+    ''' 
+    https://medium.com/distributed-computing-with-ray/anatomy-of-a-custom-environment-for-rllib-327157f269e5
+
+    https://towardsdatascience.com/ray-and-rllib-for-fast-and-parallel-reinforcement-learning-6d31ee21c96c
+    env_name = 'MyEnv-v0'
+config = {
+    # Whatever config settings you'd like...
+    }
+trainer = agents.ppo.PPOTrainer(
+    env=env_creator(env_name), 
+    config=config)
+max_training_episodes = 10000
+while True:
+    results = trainer.train()
+    # Enter whatever stopping criterion you like
+    if results['episodes_total'] >= max_training_episodes:
+        break
+print('Mean Rewards:\t{:.1f}'.format(results['episode_reward_mean']))'''
